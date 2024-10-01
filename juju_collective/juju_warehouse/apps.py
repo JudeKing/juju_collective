bf925324b59d604949e46c3cd65e4401e10c8437
JudeKing <judecoding@gmail.com>
2024-10-01 11:46:19 +0200

    Adding detailed docstrings to the apps.py file for registering the Warehouse application with JujuWarehouseConfig

--- juju_collective/juju_warehouse/apps.py
+++ juju_collective/juju_warehouse/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class JujuWarehouseConfig(AppConfig):
+    """Configuration class for the JUJU Warehouse application.
+        
+        Attributes:
+            default_auto_fields (str): The default field type.
+            name (str): The name of the application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'juju_warehouse'
