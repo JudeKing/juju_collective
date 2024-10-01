c32d302c7063b9b98d162f5617a70692896b087d
JudeKing <judecoding@gmail.com>
2024-10-01 11:41:58 +0200

    Adding detailed docstrings to the models.py file for the Product class

--- juju_collective/juju_warehouse/models.py
+++ juju_collective/juju_warehouse/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    """Represents a product with its name, description, price and image.
+        
+        Attributes:
+            product_name (str): The name of the product.
+            product_description (str): The description of the product.
+            product_price (int): The price of the product.
+            product_image (ImageField): The image of the product.
+
+        Methods:
+            __str__(): Returns the name of the product.
+    """
     product_name = models.CharField(max_length=200)
     product_description = models.CharField(max_length=600)
     product_price = models.PositiveIntegerField(default=0)
